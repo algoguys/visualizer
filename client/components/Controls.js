a6ee1b49df8c166cf2d27df4aa894b68931d9c9f
kmshoell <34169711+kmshoell@users.noreply.github.com>
2021-02-28 06:05:02 -0500

    feat(reset board) added buttons to clear visited nodes and reset the board

--- client/components/Controls.js
+++ client/components/Controls.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from 'react-redux'
-import { updateStatus } from '../store/grid'
+import { updateStatus, makeGrid } from '../store/grid'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlay } from '@fortawesome/free-solid-svg-icons'
 import DepthFirstSearch from '../algorithms/depthFirst'
@@ -36,72 +36,77 @@ const Controls = (props) => {
   }, [selectedAlgorithm]); // Only re-run the effect if selectedAlgorithm changes
 
 
-  const updateCell = useDispatch()
-  const dispatchRunningTrue = useDispatch()
-  const dispatchRunningFalse = useDispatch()
+  const updateCell = useDispatch();
+  const dispatchRunningTrue = useDispatch();
+  const dispatchRunningFalse = useDispatch();
+  const resetBoard = useDispatch();
+
+  const clearVisitedNodes = () => {
+    //sorted array of properties in grid that correspond to node ids
+    let keys = Object.keys(grid).sort((a, b) => a - b)
+
+    //loops through keys to update status on each node before executing search
+    keys.forEach((nodeId, idx) => {
+      if(idx < keys.length - 2) {
+        updateCell(updateStatus(nodeId, 'unvisited'))
+      }
+    })
+  }
+
   const handleRun = () => {
     if (running.isRunning === false) {
       dispatchRunningTrue(setRunningTrue())
 
       console.log('calling DFS Traversal!')
 
-          //sorted array of properties in grid that correspond to node ids
-          let keys = Object.keys(grid).sort((a, b) => a - b)
+      clearVisitedNodes();
 
-          //loops through keys to update status on each node before executing search
-          keys.forEach((nodeId, idx) => {
-            if(idx < keys.length - 2) {
-              updateCell(updateStatus(nodeId, 'unvisited'))
-            }
-          })
+      const results = findDestination.run()
+      console.log('results', results)
 
 
-          const results = findDestination.run()
-          console.log('results', results)
+      // Use setTimeout to
+      results.visited.forEach((nodeId, idx) => {
+        console.log(speed)
+        setTimeout(() => {
+          //console.log('visited', nodeId)
+          updateCell(updateStatus(nodeId, 'visited'))
+          //console.log(nodeId, 'type updated to', grid[nodeId].type)
+          if(idx === results.visited.length - 1 && results.shortestPath.length === 0) dispatchRunningFalse(setRunningFalse())
+        }, idx * speed) //?! update time to tie to speed var on state
 
+        //idx 0 = 0 timeout
+        //idx 1 = 500 timeout
+        //idx 2 = 1000 timeout
+        //...
+        //idx 10 = 5000 timeout
+      })
 
-          // Use setTimeout to
-          results.visited.forEach((nodeId, idx) => {
-            console.log(speed)
-            setTimeout(() => {
-              //console.log('visited', nodeId)
-              updateCell(updateStatus(nodeId, 'visited'))
-              //console.log(nodeId, 'type updated to', grid[nodeId].type)
-              if(idx === results.visited.length - 1 && results.shortestPath.length === 0) dispatchRunningFalse(setRunningFalse())
-            }, idx * speed) //?! update time to tie to speed var on state
+      //?! I don't like the solution below, it feels clunky.  I'd rather use async/await or promises
 
-            //idx 0 = 0 timeout
-            //idx 1 = 500 timeout
-            //idx 2 = 1000 timeout
-            //...
-            //idx 10 = 5000 timeout
-          })
+      //?! we need to add a var for isRunning on state to ensure we can only run the algo if it's not already being run.
 
-          //?! I don't like the solution below, it feels clunky.  I'd rather use async/await or promises
+      //?! we need a way to clear the board of visited nodes before we start an algorithm for the second time
 
-          //?! we need to add a var for isRunning on state to ensure we can only run the algo if it's not already being run.
+      //outer timeout delays call until after visited nodes have been traversed
+      setTimeout( () => {
+        results.shortestPath.forEach((nodeId, idx) => {
+          setTimeout(() => { // controls timeout for shortestPath
+            //console.log('shortestPath', nodeId)
+            updateCell(updateStatus(nodeId, 'shortestPath'))
 
-          //?! we need a way to clear the board of visited nodes before we start an algorithm for the second time
+            console.log('results', results)
 
-          //outer timeout delays call until after visited nodes have been traversed
-          setTimeout( () => {
-            results.shortestPath.forEach((nodeId, idx) => {
-              setTimeout(() => { // controls timeout for shortestPath
-                //console.log('shortestPath', nodeId)
-                updateCell(updateStatus(nodeId, 'shortestPath'))
+            //console.log(nodeId, 'type updated to', grid[nodeId].type)
+            if(idx === results.shortestPath.length - 1) dispatchRunningFalse(setRunningFalse())
 
-                console.log('results', results)
+          }, idx * speed) //?! update time to tie to speed var on state
+        })
 
-                //console.log(nodeId, 'type updated to', grid[nodeId].type)
-                if(idx === results.shortestPath.length - 1) dispatchRunningFalse(setRunningFalse())
+      }, results.visited.length * speed )
 
-              }, idx * speed) //?! update time to tie to speed var on state
-            })
-
-          }, results.visited.length * speed )
-
-          //console.log('start and end', grid.start, grid.end)
-          //depthFirst(grid.start, grid.end);
+      //console.log('start and end', grid.start, grid.end)
+      //depthFirst(grid.start, grid.end);
     } else {
       console.log('already running')
     }
@@ -137,6 +142,22 @@ const Controls = (props) => {
 
         </select>
       </label>
+
+      {/* reset board  */}
+      <button onClick={() => {
+        //?! Poor practice: make width and heigh global state variables
+        const width = Math.floor(document.getElementById('main').offsetWidth/25)
+        const height = Math.floor((window.innerHeight-275)/25)
+        resetBoard(makeGrid(width, height))
+        }}>
+        Reset Board
+      </button>
+
+      {/* clear visited nodes */}
+      <button onClick={() => clearVisitedNodes()}>
+        Clear Visited Nodes
+      </button>
+
     </div>
 
   )
